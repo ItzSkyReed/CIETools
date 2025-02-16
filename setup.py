a49008f504f1c6927f23f8c59d2c73e9be4b00a7
SkyReed <itzskyreed@gmail.com>
2025-02-16 20:25:20 +0500

    bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ module = Extension(
 
 setup(
     name='cietools',
-    version='0.1',
+    version='0.2.1',
     description='cietools is a library to work with CIE format',
     ext_modules=[module],
     packages=find_packages(),
