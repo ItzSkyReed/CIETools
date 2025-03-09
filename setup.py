c8a7eda65380cd2a37fdf0941949df39ae216d9e
SkyReed <itzskyreed@gmail.com>
2025-03-09 11:29:18 +0500

    revert setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,8 @@ import sys
 is_msvc = sys.platform == "win32"
 if is_msvc:
     extra_compile_args = ['/std:c11', '/O2', '/fp:fast']
-    extra_link_args = []
 else:
     extra_compile_args = ['-std=c11', '-O3', '-ffast-math']
-    extra_link_args = ['-lm']
 
 module = Extension(
     'cietools._cietools_c_ext',
@@ -29,7 +27,7 @@ module = Extension(
         'src/cietools/py_operations/common.c'
     ],
     extra_compile_args=extra_compile_args,
-    extra_link_args=extra_link_args
+    extra_link_args=[]
 )
 
 
